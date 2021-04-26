2e4287e7c10d9b2165f48d77cd598046df724485
Csprier <csprier@gmail.com>
2021-04-26 15:17:28 -0400

    Created array-reference branch

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Sub optimal code: This uses Typescripts 'representation' of what an 'Object Type' looks like, 
 //   to help Javascript understand the objects you're workign with
-var person = {
-    name: 'cpdev',
-    age: 31
-};
-console.log(person.name);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'cpdev',
+//   age: 31
+// };
+// console.log(person.name);
 // Optimal, using Type Inference.
 var person2 = {
     name: 'cpdev2',
-    age: 31
+    age: 31,
+    hobbies: ['coding', 'BDO', 'pen spinning'] // Typescript infers 'string[]'
 };
-console.log(person2.name);
+var favoriteActivities; // You can explicitly declare a variable and give it the Type Array full of strings: string[]
+favoriteActivities = ['magnets', 'people\'s knees'];
+// console.log(person2.name);
+console.log(favoriteActivities);
--- index.ts
+++ index.ts
@@ -1,17 +1,24 @@
 // Sub optimal code: This uses Typescripts 'representation' of what an 'Object Type' looks like, 
 //   to help Javascript understand the objects you're workign with
-const person: {
-  name: string;
-  age: number;
-} = {
-  name: 'cpdev',
-  age: 31
-};
-console.log(person.name);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'cpdev',
+//   age: 31
+// };
+// console.log(person.name);
 
 // Optimal, using Type Inference.
 const person2 = {
   name: 'cpdev2',
-  age: 31
+  age: 31,
+  hobbies: ['coding', 'BDO', 'pen spinning'] // Typescript infers 'string[]'
 };
-console.log(person2.name);
+
+let favoriteActivities: string[]; // You can explicitly declare a variable and give it the Type Array full of strings: string[]
+favoriteActivities = ['magnets', 'people\'s knees'];
+
+// console.log(person2.name);
+console.log(favoriteActivities);
+
