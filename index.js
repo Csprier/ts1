72d04a998d42ad6ff53b9b7fb960da04a56490fa
Csprier <csprier@gmail.com>
2021-04-26 11:23:01 -0400

    Added TS Object notation and examples of optimal code

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-  /* This is the error gateway you'd write to prevent the input.values from being typeof 'string' and concatenating, instead of having the mathematical operation run on them.
-      if (typeof num1 === "number" && typeof num2 === "number") {
-        return num1 + num2;
-      } else {
-        return +num1 + +num2;
-      }
-    */
-  return num1 + num2;
-}
-button.addEventListener("click", function () {
-  /** Using + in front of input.value will make the 'string' into a number, if the string is already in number format; otherwise NaN */
-  console.log(add(+input1.value, +input2.value));
-});
+// Sub optimal code: This uses Typescripts 'representation' of what an 'Object Type' looks like, 
+//   to help Javascript understand the objects you're workign with
+var person = {
+    name: 'cpdev',
+    age: 31
+};
+console.log(person.name);
+// Optimal, using Type Inference.
+var person2 = {
+    name: 'cpdev2',
+    age: 31
+};
+console.log(person2.name);
--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
-const person = {
+// Sub optimal code: This uses Typescripts 'representation' of what an 'Object Type' looks like, 
+//   to help Javascript understand the objects you're workign with
+const person: {
+  name: string;
+  age: number;
+} = {
   name: 'cpdev',
   age: 31
 };
+console.log(person.name);
+
+// Optimal, using Type Inference.
+const person2 = {
+  name: 'cpdev2',
+  age: 31
+};
+console.log(person2.name);
